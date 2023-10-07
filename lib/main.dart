3a7e96125c36d9d62e6cfc5a53c8438beec06533
Shashank-Sahu8 <shashanksahu87070@gmail.com>
2023-10-07 15:01:26 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:my_portfolio/navbar.dart';
+import 'package:my_portfolio/splashs_screen.dart';
 void main()
 {
   runApp(Myapp());
@@ -9,8 +10,61 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.pink),
+        home:SplashScreen()
+    );
   }
 }
+
+class thispage extends StatefulWidget {
+  const thispage({super.key});
+
+  @override
+  State<thispage> createState() => _thispageState();
+}
+
+class _thispageState extends State<thispage> {
+
+  int counter=0;
+
+  void _incrementCounter() {
+    setState(() {
+      counter++;
+    });
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.black12,
+      drawer: NavBar(),
+      appBar: new AppBar(),
+      body:new Container(
+
+        ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          new Container(
+            padding:EdgeInsets.all(10),
+            child:new Text("$counter like",style: TextStyle(color: Colors.white,fontSize: 17),),
+          ),
+      new Container(
+              
+            height: 30,
+            width: 30,
+            child: new FloatingActionButton(
+                onPressed: _incrementCounter,
+                child: Icon(Icons.thumb_up),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 
 
---
+++ lib/splashs_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 4), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> thispage(),
+      ));
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:new Container(
+        color: Colors.pink[500],
+        child: new Center(
+          child: new Text("Portfolio",style: TextStyle(fontFamily: 'PermanentMarker-Regular',fontSize: 50,fontWeight: FontWeight.bold,shadows: [Shadow(blurRadius: 10.0,color: Colors.blueGrey,offset: Offset(1.0,1.0))]),),
+        ),
+      )
+    );
+  }
+}
